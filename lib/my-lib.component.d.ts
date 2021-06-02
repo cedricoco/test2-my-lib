c4e62d1fb9dadf02a7f919696e0e8f0f19151163
cedricoco <cedric.pinard.info@gmail.com>
2021-06-02 14:47:27 +0200

    testlib

--- lib/my-lib.component.d.ts
+++ lib/my-lib.component.d.ts
@@ -7,6 +7,11 @@ export interface Data {
     style: "line" | "area" | "both";
     interpolation: "linear" | "step";
 }
+export interface DATA<T> {
+    timestamp: number;
+    value: T;
+    sensorId: string;
+}
 export declare function parseBool(s: string): 1 | 0 | -1;
 export declare class MyLibComponent implements OnInit {
     private renderer;
